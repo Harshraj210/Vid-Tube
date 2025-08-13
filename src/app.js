e7c4f6d64dd0a4a64edc6e863b02993cd3ca1da7
harsh <nh1750501@gmail.com>
2025-08-13 13:40:50 +0530

    added aync files

--- src/app.js
+++ src/app.js
@@ -19,5 +19,5 @@ app.use(
 // restricts max JSON  size`
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
-app.use(express.static("public"))
+app.use(express.static("public"));
 export { app };
