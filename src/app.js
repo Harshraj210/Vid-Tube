3aaa5a574f274e6bf68b08dd1407ac028da75381
harsh <nh1750501@gmail.com>
2025-08-14 16:56:52 +0530

    added cookies-parser

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import dotenv from "dotenv";
 import express from "express";
+import cookieParser from "cookie-parser";
 
 // it normally blocks the req. if the front and backend are on different ports
 import cors from "cors";
@@ -20,6 +21,7 @@ app.use(
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
+app.use(cookieParser())
 
 
 import healthcheckroutes from "./routes/healthcheckroutes.js"
