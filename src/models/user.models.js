69f55a14077e198773a6abf6092680446234e92b
harsh <nh1750501@gmail.com>
2025-08-14 16:09:46 +0530

    added password encrytion

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,50 +1,67 @@
 import mongoose, { Schema } from "mongoose";
-const userSchema = new Schema({
-  // MDB automatically adds _id field in database -->no need of adding that
-  username: {
-    type: String,
-    required: true,
-    unique: true,
-    lowercase: true,
-    trim: true,
-    index: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    lowercase: true,
-    trim: true,
-  },
-  fullname: {
-    type: String,
-    required: true,
-
-    trim: true,
-    index: true,
-  },
-  avatar: {
-    type: String,
-    required: true,
-  },
-  coverImage: {
-    type: String,
-  },
-  watchHistory: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: "",
+import bcrypt, { compare } from "bcrypt";
+const userSchema = new Schema(
+  {
+    // MDB automatically adds _id field in database -->no need of adding that
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true,
     },
-  ],
-  password: {
-    type: string,
-    required: [true, "password is required"],
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    fullname: {
+      type: String,
+      required: true,
+
+      trim: true,
+      index: true,
+    },
+    avatar: {
+      type: String,
+      required: true,
+    },
+    coverImage: {
+      type: String,
+    },
+    watchHistory: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "",
+      },
+    ],
+    password: {
+      type: string,
+      required: [true, "password is required"],
+    },
+    refreshToken: {
+      type: string,
+    },
   },
-  refreshToken: {
-    type:string
-  },
-},
-  { timestamps : true }
+  { timestamps: true }
 );
+if (!this.modified("password")) return next()
+  // before saving data to MDB first schema method checks wheather it is correct --> bcrypt hasing library  {ig DB is hacked --> hashing scrambles the paasword }
+userSchema.pre("save", async function (next) {
+  this.password = bcrypt.hash(this.password, 10);
+  // continue savbing
+  next();
+});
+
+userSchema.methods.isPasswordcorrect=async function(password){
+  return await bcrypt.compare(password,this.password)
+}
+
+userSchema.methods.generateAccesstoken= function (){
+  
+}
 // this creates the user model in database if it not exist which will import schema from --> fro userSchmea
 export const User = mongoose.model("User", userSchema);
