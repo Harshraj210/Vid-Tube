49e542842a3533301fced0240c168dba07df8b7b
harsh <nh1750501@gmail.com>
2025-08-15 13:10:20 +0530

    updated code base

--- src/controllers/user-controller.js
+++ src/controllers/user-controller.js
@@ -1,6 +1,7 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { apiError } from "./utils/apiError.js";
 import { user } from "./model/user.model.js";
+import { uploadonCloudinary } from "./utils/cloudinary.js";
 
 const registerUser = asyncHandler(async (req, res) => {
   const { fullname, email, username, password } = req.body;
@@ -20,6 +21,11 @@ const registerUser = asyncHandler(async (req, res) => {
     throw new apiError(400, "User allready Exist");
   }
   const avatarLocalpath = req.file?.avatar[0]?.path;
+  const coverimageLocalpath = req.file?.coverimage[0]?.path;
+  if (!avatarLocalpath) {
+    throw new apiError(400, "Avattar file is missing");
+  }
+  const avatar = await uploadonCloudinary(avatarLocalpath);
 });
 
 export { registerUser };
