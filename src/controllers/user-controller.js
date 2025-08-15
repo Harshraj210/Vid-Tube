8f2a5289ca6fe90a17e11ede7984775296b393d7
harsh <nh1750501@gmail.com>
2025-08-15 12:52:01 +0530

    updated code base

--- src/controllers/user-controller.js
+++ src/controllers/user-controller.js
@@ -1,18 +1,25 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
-import {apiError} from "./utils/apiError.js"
+import { apiError } from "./utils/apiError.js";
+import { user } from "./model/user.model.js";
 
-const registerUser =asyncHandler(async(req,res)=>{
-  const {fullname,email,username,password}=req.body
+const registerUser = asyncHandler(async (req, res) => {
+  const { fullname, email, username, password } = req.body;
 
   // for validation
-  if(
+  if (
     // classic JS
-    [fullname,email,username,password].some((field)=>field?.trim()==="")
-  ){
-    throw new apiError(400,"All fields are required")
+    [fullname, email, username, password].some((field) => field?.trim() === "")
+  ) {
+    throw new apiError(400, "All fields are required");
   }
-})
 
-export {
-  registerUser
-}
+  const existedUser = awaituser.findOne({
+    $or: [{ username }, { email }],
+  });
+  if (existedUser) {
+    throw new apiError(400, "User allready Exist");
+  }
+  const avatarLocalpath = req.file?.avatar[0]?.path;
+});
+
+export { registerUser };
