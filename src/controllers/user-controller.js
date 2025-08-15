98a1288c1b156fadcbfd6e402ce70dd5cb5e01de
harsh <nh1750501@gmail.com>
2025-08-15 13:29:16 +0530

    updated code base

--- src/controllers/user-controller.js
+++ src/controllers/user-controller.js
@@ -47,7 +47,7 @@ const registerUser = asyncHandler(async (req, res) => {
   }
   return res
   .status(201)
-  .json(new apiResponse)
+  .json(new apiResponse(201,createdUser,"User register successfully"))
 });
 
 export { registerUser };
